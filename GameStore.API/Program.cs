0e3c71590d0b88a34de9484f523d9c8387920d1b
SESA76667 <sesa76667@se.com>
2025-07-01 00:05:44 +0200

    Add DTOs Add gme DTO record

--- GameStore.API/Program.cs
+++ GameStore.API/Program.cs
@@ -1,6 +1,33 @@
+using GameStore.API.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//------------------------------
+// Create record in memeory
+//------------------------------
+
+List<GameDto> games = [
+        new (
+            1,
+            "Street Figther II",
+            "Figthing",
+            19.99M,
+            new DateOnly(1992, 7, 14)),
+        new (
+            2,
+            "Final Fabtasy XIV",
+            "Figthing",
+            59.99M,
+            new DateOnly(2000, 10, 30)),
+        new (
+            3,
+            "FIFA 2023",
+            "Figthing",
+            69.99M,
+            new DateOnly(2022, 9, 27))
+    ];
+
+app.MapGet("/", () => games);
 
 app.Run();
