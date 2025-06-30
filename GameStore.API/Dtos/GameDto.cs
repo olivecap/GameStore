ac060ecff99aec617b7c83bdeef70fbdf7c65e6e
SESA76667 <sesa76667@se.com>
2025-07-01 00:04:59 +0200

    Add DTOS Add game Dto record

---
+++ GameStore.API/Dtos/GameDto.cs
@@ -0,0 +1,17 @@
+﻿namespace GameStore.API.Dtos
+{
+    /// <summary>
+    /// DTO for game
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <param name="Name"></param>
+    /// <param name="Genre"></param>
+    /// <param name="Price"></param>
+    /// <param name="ReleaseDate"></param>
+    public record class GameDto(
+        int Id, 
+        string Name, 
+        string Genre, 
+        decimal Price, 
+        DateOnly ReleaseDate);
+}
