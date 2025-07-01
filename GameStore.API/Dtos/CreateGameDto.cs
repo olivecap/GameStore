2b06e47f279578d024da9f9644296af9d8543b46
SESA76667 <sesa76667@se.com>
2025-07-01 21:16:24 +0200

    API Implementation Develop all primary implementation

---
+++ GameStore.API/Dtos/CreateGameDto.cs
@@ -0,0 +1,15 @@
+﻿namespace GameStore.API.Dtos
+{
+    /// <summary>
+    /// DTO for game
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Genre"></param>
+    /// <param name="Price"></param>
+    /// <param name="ReleaseDate"></param>
+    public record class CreateGameDto(
+        string Name,
+        string Genre,
+        decimal Price,
+        DateOnly ReleaseDate);
+}
---
+++ GameStore.API/Dtos/UpdateGameDto.cs
@@ -0,0 +1,20 @@
+﻿namespace GameStore.API.Dtos
+{
+    //---------------------------------------------
+    // Even if same Dto of create
+    // By convention is better to have DTO
+    // for each action in case of it will change
+    //----------------------------------------------
+    /// <summary>
+    /// DTO for game
+    /// </summary>
+    /// <param name="Name"></param>
+    /// <param name="Genre"></param>
+    /// <param name="Price"></param>
+    /// <param name="ReleaseDate"></param>
+    public record class UpdateGameDto(
+        string Name,
+        string Genre,
+        decimal Price,
+        DateOnly ReleaseDate);
+}
